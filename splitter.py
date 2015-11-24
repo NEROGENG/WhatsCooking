89cb7495f1f72b66af75e7e21c6f699b61b11bb9
NEROGENG <szmun.gengqj@gmail.com>
2015-11-24 15:06:24 -0800

    splitter works now

--- splitter.py
+++ splitter.py
@@ -11,19 +11,35 @@ def main(argv):
 	trainPercent = float(sys.argv[4])
 
 	fin = open(inputfile, 'rb')
-	reader = csv.reader(fin, delimiter=' ', quotechar='|')
+	reader = csv.reader(fin)
 
-	header = reader[0]
+	# list of lists 
+	data = list()
+	for row in reader:
+		data.append(row)
 
-	reader = reader[1:len(reader)]
-	numpy.random.shuffle(reader)
-	training, test = reader[:80,:], reader[80:,:]
+	header = data[0]
+	data.remove(data[0])
+
+	# shuffle entries
+	numpy.random.shuffle(data)
+	splitIndex = int(len(data) * trainPercent)
+
+	# split data into training set and test set
+	training, test = data[:splitIndex], data[splitIndex:]
 	fin.close()
 
 	ftrain = open(trainfile, "wb+")
-	writer = csv.writer(fout)
+	writer = csv.writer(ftrain)
 	writer.writerow(header)
 	writer.writerows(training)
+	ftrain.close()
+
+	ftest = open(testfile, "wb+")
+	writer = csv.writer(ftest)
+	writer.writerow(header)
+	writer.writerows(test)
+	ftest.close()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
