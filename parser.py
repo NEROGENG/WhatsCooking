fe292c976da45df32e9cf1b656ef0fa2199e20ec
NEROGENG <szmun.gengqj@gmail.com>
2015-11-24 01:06:36 -0800

    at least we can use the parser with command line inputs

--- parser.py
+++ parser.py
@@ -1,50 +1,57 @@
 #!/usr/bin/env python
-import sys
+import sys, getopt
 import json
 import csv
-from pprint import pprint
 
-f = open('train.json') 
-data = json.load(f) 
-f.close()
+def main(argv):
 
-# create a list of all ingredients, dups allowed
-ingredients = list()
-for i in range(0, len(data)):
-	for j in range(0, len(data[i]["ingredients"])):
-		ingredients.append(data[i]["ingredients"][j].encode("utf-8"))
+	inputfile = str(sys.argv[1])
+	outputfile = str(sys.argv[2])
 
-# number of interested frequent ingredients
-frequent = 300
+	# number of interested frequent ingredients
+	frequent = int(sys.argv[3])
 
-counter = {}
-for ingredient in ingredients:
-	if ingredient in counter:
-		counter[ingredient] += 1
-	else:
-		counter[ingredient] = 1
+	f = open(inputfile)
+	data = json.load(f) 
+	f.close()
 
-# ingredients in frequency descending order
-frequentIngredients = sorted(counter, key = counter.get, reverse = True)
+	# create a list of all ingredients, dups allowed
+	ingredients = list()
+	for i in range(0, len(data)):
+		for j in range(0, len(data[i]["ingredients"])):
+			ingredients.append(data[i]["ingredients"][j].encode("utf-8"))
 
-frequentIngredients = frequentIngredients[:frequent]
-
-# pprint(frequentIngredients)
-
-writer = csv.writer(open("test.csv", "wb+"))
-# write header
-writer.writerow(['id', 'cuisine'] + frequentIngredients)
-# write entries
-for i in range(0, len(data)):
-	row = list()
-	row.append(data[i]["id"])
-	row.append(data[i]["cuisine"].encode('utf-8'))
-
-	items =  [x.encode('utf-8') for x in data[i]["ingredients"]]
-
-	for j in range(0, len(frequentIngredients)):
-		if frequentIngredients[j] in items:
-			row.append(1)
+	counter = {}
+	for ingredient in ingredients:
+		if ingredient in counter:
+			counter[ingredient] += 1
 		else:
-			row.append(0)
-	writer.writerow(row)
+			counter[ingredient] = 1
+
+	# ingredients in frequency descending order
+	frequentIngredients = sorted(counter, key = counter.get, reverse = True)
+
+	frequentIngredients = frequentIngredients[:frequent]
+
+	# pprint(frequentIngredients)
+
+	writer = csv.writer(open(outputfile, "wb+"))
+	# write header
+	writer.writerow(['id', 'cuisine'] + frequentIngredients)
+	# write entries
+	for i in range(0, len(data)):
+		row = list()
+		row.append(data[i]["id"])
+		row.append(data[i]["cuisine"].encode('utf-8'))
+
+		items =  [x.encode('utf-8') for x in data[i]["ingredients"]]
+
+		for j in range(0, len(frequentIngredients)):
+			if frequentIngredients[j] in items:
+				row.append(1)
+			else:
+				row.append(0)
+		writer.writerow(row)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
